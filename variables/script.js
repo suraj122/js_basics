a23b139431053a5f8008daaf0ab504d104ae66a1
Suraj Kumar <122suraj0@gmail.com>
2018-12-18 00:10:41 +0530

    variables and constants

--- variables/script.js
+++ variables/script.js
@@ -10,3 +10,22 @@ alert(message)  // Shows the value stored in variables
 let msg = 'Hello World'; // Defining a variable and assigning the value at the same time
 
 alert(msg);
+
+// defining multiple variables in the same lind
+let user = 'John', age = '32', msgs = 'Hello!';
+alert(user);
+
+var greeting = "Hello World";
+
+var saying;
+saying = greeting;
+alert(saying);
+alert(greeting);
+
+
+// Constants
+
+const myBirthday = '18-07-1993';
+alert(myBirthday);
+
+// myBirthday = '1.1.1993';
