41b522f8d6a17a331059abaca7058d20eea69ecb
Suraj Kumar <122suraj0@gmail.com>
2018-12-17 23:53:28 +0530

    variables

---
+++ variables/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Variables
+let message;
+
+message = 'Hello!'  // store the string
+
+alert(message)  // Shows the value stored in variables
+
+let msg = 'Hello World'; // Defining a variable and assigning the value at the same time
+
+alert(msg);
