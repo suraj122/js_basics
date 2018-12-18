3188b2005206cc1712f95fa7f32eeb9bc3054ad9
Suraj Kumar <122suraj0@gmail.com>
2018-12-18 23:28:56 +0530

    data types

---
+++ data_types/script.js
@@ -0,0 +1,19 @@
+let name = "John";
+
+alert(`Hello${name}`);
+alert(`The result is ${1 + 2}`);
+
+let isGreaterThan = 6 > 1;
+
+alert(isGreaterThan);
+
+let x = null;
+alert(x);
+
+// Task
+let person = "Ilya";
+alert( `hello ${1}` );
+
+alert(`hello ${"person"}`);
+alert(`hello ${name}`);
+
