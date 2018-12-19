fcda4bdff680ade3b14fefe8c96e9cc340ff1cd6
Suraj Kumar <122suraj0@gmail.com>
2018-12-19 23:54:42 +0530

    type conversion

---
+++ type_conversion/script.js
@@ -0,0 +1,37 @@
+// ToString
+
+// let value = true;
+// alert(typeof value);
+
+// value = String(value);
+
+// alert(value);
+
+// // ToNumber
+
+// alert("6" / "2");
+
+// let str = "123";
+// alert(typeof(str));
+
+// let num = Number(str);
+// alert(typeof num);
+
+// let age = Number("age of sachin");
+// alert(age);
+// alert("   123   ");
+// alert (Number("123r"));
+
+// alert(Number(true));
+// alert(Number(false));
+
+// alert(Number(null));
+// alert(Number(undefined));
+
+// alert(1 + "2");
+
+// ToBoolean
+alert(Boolean(1));
+alert(Boolean("0"));
+alert(Boolean(" "));
+
