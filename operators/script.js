f29843f63a7e4e644e4c5441b164ab4fc9c063a3
Suraj Kumar <122suraj0@gmail.com>
2018-12-21 12:38:19 +0530

    operators

---
+++ operators/script.js
@@ -0,0 +1,35 @@
+alert("4" * 2);
+alert(+ "");
+
+let apple = "2";
+let orange = "3";
+alert(+apple + +orange);
+
+let count = 5;
+alert( count++ );
+
+let counter = 1;
+alert( 2 * counter);
+console.log(++counter);
+
+let n = 2;
+n += 5;
+console.log(n);
+n *= 2;
+console.log(n);
+n *= 5 + 3;
+console.log(n);
+
+// Tasks
+
+let a = 1, b = 1;
+let c = ++a;
+console.log(c);
+let d = b++;
+console.log(d);
+console.log(b);
+
+let y = 2;
+let x = 1 + (y *= 2);
+console.log(x);
+console.log(y);
