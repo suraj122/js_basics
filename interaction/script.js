ee12404671b82ae016a5b72eb688bae58fb26fe1
suraj_122 <122suraj0@gmail.com>
2019-01-22 14:47:36 +0530

    interaction

---
+++ interaction/script.js
@@ -0,0 +1,21 @@
+// Alert
+
+alert("Hello");
+
+// Prompt
+let age;
+age = prompt("How old are you?", 100);
+
+alert(`you are ${age} years old.`);
+
+// Confirm
+let isBoss = confirm('Are you the boss');
+
+alert(isBoss);
+
+
+// Create a web-page that asks for a name and outputs it.
+
+let currentUserName = prompt("what's your name?", 'Name');
+
+alert(`My name is ${currentUserName}.`);
