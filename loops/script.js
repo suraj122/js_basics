5e2729fd3df9bc250d9d0ff3d459992485ca6657
suraj_122 <122suraj0@gmail.com>
2019-01-24 18:26:32 +0530

    add some loops

--- loops/script.js
+++ loops/script.js
@@ -1,91 +1,91 @@
 'use strict';
 
-// While loops
+While loops
 
-// let i = 0;
+let i = 0;
 
-// while(i < 3) {
-// 	alert(i);
-// 	i++;
-// }
+while(i < 3) {
+	alert(i);
+	i++;
+}
 
-// while(i) alert(i--);
+while(i) alert(i--);
 
 
 // Do while
 
-// let j = 0;
+let j = 0;
 
-// do {
-// 	alert(j);
-// 	j++
-// } while(j <= 3);
+do {
+	alert(j);
+	j++
+} while(j <= 3);
 
 
 // For loop
-// let a = 0;
-// for( a = 0; a <= 3; a++) {
-// 	// alert(a);
-// }
-// alert(a);
+let a = 0;
+for( a = 0; a <= 3; a++) {
+	// alert(a);
+}
+alert(a);
 
 // Breaking the loop
 
-// let sum = 0;
-//  while (true) {
+let sum = 0;
+ while (true) {
 
-//  	let value = +prompt("Enter a value", '');
-//  	if (!value) break;
-//  	sum += value;
+ 	let value = +prompt("Enter a value", '');
+ 	if (!value) break;
+ 	sum += value;
 
-//  }
-//  alert('Sum: ' +sum);
+ }
+ alert('Sum: ' +sum);
 
-// for (let x = 0; x < 10; x++) {
+for (let x = 0; x < 10; x++) {
 
-//  	if(x % 2) {
-//  		alert(x);
-//  	}
+ 	if(x % 2) {
+ 		alert(x);
+ 	}
  	
-// }
+}
 
 
 // Labels for break/continue
 
-// outer: for (let k = 0; k < 3; k++) {
-// 	for (let l = 0; l < 3; l++) {
-// 		let input = prompt(`value at cords ${k},${l}`, '');
-// 		if (!input) break outer;
-// 	}
-// }
-// alert('done!')
+outer: for (let k = 0; k < 3; k++) {
+	for (let l = 0; l < 3; l++) {
+		let input = prompt(`value at cords ${k},${l}`, '');
+		if (!input) break outer;
+	}
+}
+alert('done!')
 
 
 // Tasks
 
-// let y = 3;
-// while(y) {
-// 	alert(y--);
-// }
+let y = 3;
+while(y) {
+	alert(y--);
+}
 
-// let z = 0;
-// while(++z < 5) alert(z);
+let z = 0;
+while(++z < 5) alert(z);
 
-// let b = 0;
-// while(b++ < 5) alert(b);
+let b = 0;
+while(b++ < 5) alert(b);
 
-// for(let c = 0; c < 5; ++c) {
-// 	alert(c);
-// }
-// for (let d = 0; d < 5; d++) {
-// 	alert(d);
-// }
+for(let c = 0; c < 5; ++c) {
+	alert(c);
+}
+for (let d = 0; d < 5; d++) {
+	alert(d);
+}
 
-// for(let e = 0; e <= 10; e++) {
-// 	if(e % 2 == 0) {
-// 		alert(e);
-// 	}
-// }
+for(let e = 0; e <= 10; e++) {
+	if(e % 2 == 0) {
+		alert(e);
+	}
+}
 
 // let f = 0;
 // while( f < 3) {
